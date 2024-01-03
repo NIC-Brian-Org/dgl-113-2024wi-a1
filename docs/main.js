b9bc6e1933b6057e7cc2604d59a152c0b787f357
koehlerb <brian@koehler.ca>
2024-01-03 14:36:41 -0800

    better error handling in main.js

--- docs/main.js
+++ docs/main.js
@@ -3,21 +3,31 @@
 (() => {
   window.addEventListener('load', (event) => {
     // update document content
-    const setContent = (id, text) => {
+    const setContent = (id, type) => {
+      const value = eval(id);
+      let text = 'Uh-oh!';
+      switch (typeof value) {
+        case 'string':
+          if (type == 'ordinary') text = value;
+          break;
+        case 'number':
+          if (type == 'ordinary') text = value;
+          if (type == 'dollar') text = '$' + value.toFixed(2);
+          if (type == 'percent') text = (value * 100).toFixed(2) + '%';
+          break;
+      }
       document.getElementById(id).textContent = text;
     };
 
     // ordinary values
-    ['guestMessage', 'numberOfNights'].map((id) => setContent(id, eval(id)));
+    ['guestMessage', 'numberOfNights'].map((id) => setContent(id, 'ordinary'));
 
     // dollar values
     ['nightlyRate', 'subTotal', 'pstAmount', 'gstAmount', 'total'].map((id) =>
-      setContent(id, '$' + eval(id).toFixed(2))
+      setContent(id, 'dollar')
     );
 
     // percent values
-    ['pstRate', 'gstRate'].map((id) =>
-      setContent(id, (eval(id) * 100).toFixed(2) + '%')
-    );
+    ['pstRate', 'gstRate'].map((id) => setContent(id, 'percent'));
   });
 })();
